05ee0cf3d4b304d7f0b71a4843648cff502de2d7
gabrlcj <gabrielbittencourt57@gmail.com>
2025-02-13 19:33:41 -0300

    feat: adds slug

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,31 @@
+export class Slug {
+  public value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  /**
+   * Creates a slug from the given text.
+   *
+   * This method takes a string of text and converts it into a URL-friendly slug.
+   * It typically involves transforming the text to lowercase, replacing spaces
+   * with hyphens, and removing any special characters.
+   *
+   * @param text - The text to be converted into a slug.
+   * @returns A new slug instance created from the provided text. Example: "Hello, World!" -> "hello-world"
+   */
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-') // Remove spaces
+      .replace(/[^\w-]+/g, '') // Remove special characters
+      .replace(/_/g, '-') // Replace underscores with hyphens
+      .replace(/--+/g, '-') // Replace double hyphens with a single hyphen
+      .replace(/-$/g, ''); // Remove trailing hyphens
+
+    return new Slug(slugText);
+  }
+}
