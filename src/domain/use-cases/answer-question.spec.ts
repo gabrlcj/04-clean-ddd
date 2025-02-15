bb750b93743d36ffaa54d54b35ac5d827af3ef9b
gabrlcj <gabrielbittencourt57@gmail.com>
2025-02-15 12:45:14 -0300

    chore: adjusts files imports

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,4 +1,3 @@
-import { expect, it } from 'vitest';
 import { AnswerQuestionUseCase } from './answer-question';
 import { AnswerRepository } from '../repositories/answer-repository';
 import { Answer } from '../entities/answer';
