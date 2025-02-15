fc476478638afeef62a2c32412253ec91a3066fb
gabrlcj <gabrielbittencourt57@gmail.com>
2025-02-15 12:27:32 -0300

    refactor: adds getters and setters for `answer.ts` and `question.ts` entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -11,8 +11,38 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
+  get questionId() {
+    return this.props.questionId;
+  }
+
+  get authorId() {
+    return this.props.authorId;
+  }
+
   get content() {
     return this.props.content;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...');
+  }
+
+  private updated() {
+    this.props.updatedAt = new Date();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+
+    this.updated();
   }
 
   static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
