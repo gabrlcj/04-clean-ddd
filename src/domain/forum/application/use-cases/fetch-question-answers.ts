269bfc9aeced5b06ae549fd3e34c45531cb3114b
gabrlcj <gabrielbittencourt57@gmail.com>
2025-02-26 19:47:44 -0300

    feat: adds fetch question answers use case

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,27 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+interface FetchQuestionAnswersParams {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionAnswersResponse {
+  answers: Answer[]
+}
+
+export class FetchQuestionAnswersUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswersParams): Promise<FetchQuestionAnswersResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      { page },
+    )
+
+    return { answers }
+  }
+}
