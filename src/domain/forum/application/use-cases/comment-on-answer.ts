648be5f0cfb2f5008beb555989a8a444345e1efc
gabrlcj <gabrielbittencourt57@gmail.com>
2025-02-27 19:20:25 -0300

    feat: adds repository and use case for `answer-comments`

---
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -0,0 +1,43 @@
+import { UniqueEntityID } from '@/core/entities/value-object/unique-entity-id'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+
+interface CommentOnAnswerParams {
+  authorId: string
+  answerId: string
+  content: string
+}
+
+interface CommentOnAnswerResponse {
+  answerComment: AnswerComment
+}
+
+export class CommentOnAnswerUseCase {
+  constructor(
+    private answersRepository: AnswersRepository,
+    private answerCommentsRepository: AnswerCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    answerId,
+    content,
+  }: CommentOnAnswerParams): Promise<CommentOnAnswerResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    const answerComment = AnswerComment.create({
+      authorId: new UniqueEntityID(authorId),
+      answerId: new UniqueEntityID(answerId),
+      content,
+    })
+
+    await this.answerCommentsRepository.create(answerComment)
+
+    return { answerComment }
+  }
+}
