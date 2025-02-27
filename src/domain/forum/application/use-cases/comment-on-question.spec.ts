dd7e2b4df33873428ae0298cf9b56aad920d8473
gabrlcj <gabrielbittencourt57@gmail.com>
2025-02-27 19:15:05 -0300

    test: adds test for comment on question use case

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { CommentOnQuestionUseCase } from './comment-on-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+// sut (system under test)
+let sut: CommentOnQuestionUseCase
+
+describe('Comment On Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  it('should be able to create a comment on a question', async () => {
+    const question = makeQuestion()
+
+    await inMemoryQuestionsRepository.create(question)
+
+    const { questionComment } = await sut.execute({
+      authorId: 'author-id',
+      questionId: question.id.toString(),
+      content: 'New content',
+    })
+
+    expect(questionComment.id).toBeTruthy()
+    expect(inMemoryQuestionCommentsRepository.items[0].id).toEqual(
+      questionComment.id,
+    )
+  })
+})
