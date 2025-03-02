db46b48e355ec9f99ce4eebce385836c2ca392e6
gabrlcj <gabrielbittencourt57@gmail.com>
2025-03-02 13:02:12 -0300

    refactor: adjusts use cases and entity to use correctly answer attachment watched list

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -2,10 +2,13 @@ import { UniqueEntityID } from '@/core/entities/value-object/unique-entity-id'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { Either, right } from '@/core/either'
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
 
 interface AnswerQuestionParams {
   instructorId: string
   questionId: string
+  attachmentIds: string[]
   content: string
 }
 
@@ -22,6 +25,7 @@ export class AnswerQuestionUseCase {
   async execute({
     instructorId,
     questionId,
+    attachmentIds,
     content,
   }: AnswerQuestionParams): Promise<AnswerQuestionResponse> {
     const answer = Answer.create({
@@ -29,6 +33,15 @@ export class AnswerQuestionUseCase {
       authorId: new UniqueEntityID(instructorId),
       questionId: new UniqueEntityID(questionId),
     })
+
+    const answerAttachments = attachmentIds.map((id) => {
+      return AnswerAttachment.create({
+        answerId: answer.id,
+        attachmentId: new UniqueEntityID(id),
+      })
+    })
+
+    answer.attachments = new AnswerAttachmentList(answerAttachments)
 
     await this.answersRepository.create(answer)
 
