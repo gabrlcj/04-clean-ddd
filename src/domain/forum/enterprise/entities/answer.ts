9a027c86da61b207c8182173c0fd154f9e06212d
gabrlcj <gabrielbittencourt57@gmail.com>
2025-03-03 19:23:34 -0300

    feat: adds event  to when a question is answered

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,7 +1,8 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/value-object/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import { AnswerAttachmentList } from './answer-attachment-list'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
+import { AnswerCreatedEvent } from '../events/answer-created-event'
 
 export interface AnswerProps {
   questionId: UniqueEntityID
@@ -12,7 +13,7 @@ export interface AnswerProps {
   updatedAt?: Date
 }
 
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends AggregateRoot<AnswerProps> {
   get questionId() {
     return this.props.questionId
   }
@@ -69,6 +70,10 @@ export class Answer extends Entity<AnswerProps> {
       },
       id,
     )
+
+    const isNewAnswer = !id
+
+    if (isNewAnswer) answer.addDomainEvent(new AnswerCreatedEvent(answer))
 
     return answer
   }
